7680bb57d1e0faa4022fcc40900cefba5d456e08
digmen <digmen68@gmail.com>
2024-06-30 15:08:16 +0000

    sdfsdfdsf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,18 +10,18 @@ export default function Footer() {
 
     return (
         <div className='flex justify-between px-[40px] pt-[35px]  max-sm:flex-col  max-sm:pb-12 bg-[#000000a4]' style={{ backgroundImage: `url(${footerBg})`, backgroundSize: 'cover' }}>
-            <div className='flex flex-col pb-[14px] gap-[1px]  max-sm:items-center'>
+            <div className='flex flex-col pb-[14px] gap-[1px]  max-sm:items-center w-[300px]'>
                 <Link to='/' className='text-white text-[20px] font-bold'>{t('header.homeLink')}</Link>
                 <Link to='/about' className='text-white text-[20px] font-bold'>{t('header.homeLink2')}</Link>
                 <Link to='/product' className='text-white text-[20px] font-bold'>{t('header.homeLink3')}</Link>
                 <Link to='/service' className='text-white text-[20px] font-bold'>{t('header.homeLink4')}</Link>
                 <Link to='/contact' className='text-white text-[20px] font-bold'>{t('header.homeLink5')}</Link>
             </div>
-            <div className='flex items-center flex-col gap-4'>
+            <div className='flex items-center flex-col gap-4 w-[300px]'>
                 <img src={logo} alt='logo' className='w-[77px] h-[69px]' />
                 <Link to='/' className='text-white text-[30px] font-bold'>ALPHA IMPEX</Link>
             </div>
-            <div className='flex  max-sm:flex-col  max-sm:gap-10  max-sm:items-center'>
+            <div className='flex  max-sm:flex-col  max-sm:gap-10  max-sm:items-center w-[300px]'>
                 <div className='w-[290px] flex items-center flex-col'>
                     <div className='border border-white p-2 w-[210px]'>
                         <a href='mailto:alphaimpex.org@outlook.com' className='text-white text-[14px] font-bold '>alphaimpex.org@outlook.com</a>
