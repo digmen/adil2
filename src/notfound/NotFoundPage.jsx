05e3b009e1fed9b8f01a72cc0f76b4f47787c557
digmen <digmen68@gmail.com>
2024-07-15 11:40:59 +0000

    sdfsdfsdf

--- src/notfound/NotFoundPage.jsx
+++ src/notfound/NotFoundPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import logo from '../components/images/logo.svg'
 
+import warningImg from './images/warning.png'
+
 
 export default function NotFoundPage() {
     return (
         <div className='flex flex-col items-center'>
-            <div></div>
-            <div className='flex flex-col items-center gap-10'>
+            <div className='mt-[50px]'>
+                <img src={warningImg} />
+            </div>
+            <div className='flex flex-col items-center gap-10 mt-[20px]'>
                 <img className='w-[160px] h-[190px]' src={logo} alt='logo' />
                 <span className='font-black text-[32px]'>We apologize for the inconvenience, technical work is currently underway, please wait</span>
             </div>
